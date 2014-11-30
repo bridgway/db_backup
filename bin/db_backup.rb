fd1e8283ecff42d27c063a63d517312ba301cf90
Ben Ridgway <benridgway83@gmail.com>
2014-11-30 19:49:25 +0000

    add option parser

--- bin/db_backup.rb
+++ bin/db_backup.rb
@@ -1,21 +1,50 @@
 #!/usr/bin/env ruby
 
-database = ARGV.shift
-username = ARGV.shift
-password = ARGV.shift
-end_of_iter = ARGV.shift
+# Bring OptionParser into the namespace
 
-if end_of_iter.nil?
-	backup_file = database + Time.now.strftime("%Y%m%d")
-else
-	backup_file = database + end_of_iter
+require 'optparse'
+
+options = {}
+option_parser = OptionParser.new do |opts| 
+
+	# create a switch
+	opts.on("-i", "--iteration") do
+		options[:interation] = true
+	end
+
+	# crate a flag
+	opts.on("-u USER") do |user|
+		unless user =~ /^.+\..+$/
+			raise ArgumentError, "USER must be in 'first.last' format"
+		end
+		options[:user] = user
+	end
+
+	opts.on("-p PASSWORD") do |password|
+		options[:password] = password
+	end
+
 end
 
-Dir.chdir("/usr/local/mysql/bin") do
-  `./mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
-end
+option_parser.parse!
+puts options.inspect 
 
-puts `pwd`
+# database = ARGV.shift
+# username = ARGV.shift
+# password = ARGV.shift
+# end_of_iter = ARGV.shift
 
-#`mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
-`gzip #{backup_file}.sql`
+# if end_of_iter.nil?
+# 	backup_file = database + Time.now.strftime("%Y%m%d")
+# else
+# 	backup_file = database + end_of_iter
+# end
+
+# Dir.chdir("/usr/local/mysql/bin") do
+#   `./mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
+# end
+
+# puts `pwd`
+
+# ##`mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
+# `gzip #{backup_file}.sql`
