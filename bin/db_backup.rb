ef1c19191e8bdd8e7563de41de81a5965596bcd5
Ben Ridgway <benridgway83@gmail.com>
2014-12-29 15:27:52 +0900

    add error handling

--- bin/db_backup.rb
+++ bin/db_backup.rb
@@ -3,16 +3,18 @@
 # Bring OptionParser into the namespace
 
 require 'optparse'
+require 'open3'
+require 'fileutils'
 
 options = {}
 option_parser = OptionParser.new do |opts| 
 
 	executable_name = File.basename($PROGRAM_NAME)
-	opts.banner = 
-"Backup one or more MySQL databases
+	opts.banner = <<-EOS
+Backup one or more MySQL databases
 
 Usage: #{executable_name} [options] database_name
-"
+EOS
 
 	# create a switch
 	opts.on("-i", "--iteration", 
@@ -37,34 +39,88 @@ Usage: #{executable_name} [options] database_name
 	end
 end
 
-option_parser.parse!
-
-if ARGV.empty?
-	puts "error: you must supply a database name"
-	puts
-	puts option_parser.help
-else
-	database_name = ARGV[0]
-	# proceed as normal to backup database_name
+begin
+	option_parser.parse!
+	if ARGV.empty?
+		puts "error: you must supply a database name"
+		puts
+		puts option_parser.help
+		exit 2
+	end
+rescue OptionParser::InvalidArgument => e
+	STDERR.puts ex.message
+	STDERR.puts option_parser
+	exit 1
 end
-puts options.inspect 
 
-# database = ARGV.shift
-# username = ARGV.shift
-# password = ARGV.shift
-# end_of_iter = ARGV.shift
+auth = ""
+auth += "-u#{options[:user]} " if options[:user]
+auth += "-p#{options[:password]} " if options[:password]
+
+database_name = ARGV[0]
 
 if options[:interation] == false
-	backup_file = options[:database] + Time.now.strftime("%Y%m%d")
+	output_file = database_name + Time.now.strftime("%Y%m%d") + ".sql"
 else
-	backup_file = options[:database] + "end_of_interation"
+	output_file = database_name + "end_of_interation" + ".sql"
 end
 
-Dir.chdir("/usr/local/mysql/bin") do
-  `./mysqldump -u#{options[:user]} -p#{options[:password]} #{options[:database]} > #{backup_file}.sql`
+Signal.trap("SIGINT") do
+	FileUtils.rm output_file
+	exit 1
+end 
+
+command = "/usr/local/mysql/bin/mysqldump #{auth}#{database_name} > #{output_file}"
+
+
+if ENV['NO_RUN']
+  def system(cmd) # not command?
+    puts cmd
+    true
+  end
+end
+if false
+system(command)
 end
 
-# puts `pwd`
+puts "Running '#{command}'"
+stdout_str, stderr_str, status = Open3.capture3(command)
+
+unless status.success?
+  STDERR.puts "There was a problem running '#{command}'"
+  STDERR.puts stderr_str.gsub(/^mysqldump: /,'')
+  exit 1
+end
+
+
+# ----
+
+# option_parser.parse!
+
+# if ARGV.empty?
+# 	puts "error: you must supply a database name"
+# 	puts
+# 	puts option_parser.help
+# else
+# 	database_name = ARGV[0]
+# 	# proceed as normal to backup database_name
+# end
+# puts options.inspect 
+
+# ----
+
+# # not sure if should take db name like this?
+# if options[:interation] == false
+# 	backup_file = options[:database] + Time.now.strftime("%Y%m%d")
+# else
+# 	backup_file = options[:database] + "end_of_interation"
+# end
+
+# # not sure if need to run command like this as file might not be executable??
+# Dir.chdir("/usr/local/mysql/bin") do
+#   `./mysqldump -u#{options[:user]} -p#{options[:password]} #{options[:database]} > #{backup_file}.sql`
+# end
+
 
 # `mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
-`gzip #{backup_file}.sql`
+`gzip #{output_file}.sql`
